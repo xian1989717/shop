0f7bb87f13b8ef63aae779f376c2fe25795432c5
xian1989717 <383883040@qq.com>
2018-05-20 22:07:59 +0800

    实现功能

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,13 +3,13 @@
     <mt-search class="myHeight" v-model="value" cancel-text="取消" placeholder="搜索"></mt-search>
     <main>
       <div>{{meg}}</div>
-      <ul>
-         <li class="first_li" v-for="item in list">
+      <ul class="myUl">
+         <li class="first_li" v-for="item in list" @click="toTest(item.id)">
            <div class="list_div_left">
              <img :src="item.img">
            </div>
            <div class="list_div_right">
-            <div>{{item.name}}</div>
+            <div style="margin:10px 0 10px 0">{{item.name}}</div>
             <div>{{item.groupPrice}}</div>
             <div>{{item.signPrice}}</div>
            </div>
@@ -20,52 +20,63 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import { Search } from 'mint-ui'
+import Vue from "vue";
+import { Search } from "mint-ui";
 
-Vue.component(Search.name, Search)
+Vue.component(Search.name, Search);
 export default {
-  data () {
+  data() {
     return {
-      value: '',
+      value: "",
       list: [
         {
-          img: require('../assets/01.png'),
-          name: '鹿听茶',
-          groupPrice: 20.00,
-          signPrice: 23.00
+          id: 1,
+          img: require("../assets/01.png"),
+          name: "鹿听茶",
+          groupPrice: 20.0,
+          signPrice: 23.0
         },
         {
-          img: require('../assets/01.png'),
-          name: '鹿听茶',
-          groupPrice: 20.00,
-          signPrice: 23.00
+          id: 2,
+          img: require("../assets/01.png"),
+          name: "鹿听茶",
+          groupPrice: 20.0,
+          signPrice: 23.0
         },
         {
-          img: require('../assets/01.png'),
-          name: '鹿听茶',
-          groupPrice: 20.00,
-          signPrice: 23.00
+          id: 3,
+          img: require("../assets/01.png"),
+          name: "鹿听茶",
+          groupPrice: 20.0,
+          signPrice: 23.0
         }
       ]
+    };
+  },
+  methods: {
+    toTest(id) {
+      console.log(id);
     }
   }
-
-}
+};
 </script>
 
 <style lang="less" scoped>
 .myHeight {
   height: auto;
 }
-.first_li {
-  .list_div_left {
-    width: 40%;
-    display:inline-block;
-  }
-  .list_div_right {
-    > div {
-      text-align: left;
+.myUl {
+  text-align: left;
+  .first_li {
+    overflow: hidden;
+    .list_div_left {
+      display: inline-block;
+    }
+    .list_div_right {
+      display: inline-block;
+      > div {
+        text-align: left;
+      }
     }
   }
 }
