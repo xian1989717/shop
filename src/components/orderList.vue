69beedcf55963208af0c09577fd22fd12553671c
xian1989717 <383883040@qq.com>
2018-06-08 20:47:06 +0800

    完善订单列表

--- src/components/orderList.vue
+++ src/components/orderList.vue
@@ -1,14 +1,91 @@
 <template>
-    <div>
-        订单列表
+  <div>
+    <mt-header fixed title="订单列表">
+      <router-link to="/me" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+    </mt-header>
+    <div class="nav">
+      <span v-for="item in headerList" :key="item.id" @click="changeList(item.id)">{{item.name}}</span>
     </div>
+    <div>
+      <ul>
+        <li>
+          <p>
+            <span>订单编号:1232141231313</span>
+            <span>交易关闭</span>
+          </p>
+          <div class="content-warp clearfloat">
+            <img class="fl" src="../assets/01.png" alt="">
+            <div class="clearFloat">
+              <p>
+                <span>鹿听冬瓜荷叶茶60克</span>
+                <span class="fr">￥23.00</span>
+              </p>
+              <span class="fr">X1</span>
+            </div>
+
+          </div>
+          <p class="tr">合计:￥23.00</p>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Vue from "vue";
+import { Header } from "mint-ui";
+Vue.component(Header.name, Header);
+export default {
+  data() {
+    return {
+      headerList: [
+        {
+          id: 1,
+          name: "代付款"
+        },
+        {
+          id: 2,
+          name: "代发货"
+        },
+        {
+          id: 3,
+          name: "已发货"
+        },
+        {
+          id: 4,
+          name: "已完成"
+        }
+      ]
+    };
+  },
+  methods: {
+    changeList(id) {}
+  }
+};
 </script>
 
 <style lang="less" scoped>
+.nav {
+  text-align: center;
+  line-height: 40px;
+  span {
+    width: 25%;
+    display: inline-block;
+  }
+}
+.content-warp {
+  background: #ccc;
+  margin: 5px 0;
+  padding: 5px;
+  img {
+    width: 100px;
+  }
+  div {
+    margin-left: 100px;
+  }
+}
 </style>
 
 
