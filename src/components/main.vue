444e36dbeab191ebdd5d2dd14f7a624947000330
xian1989717 <383883040@qq.com>
2018-06-21 22:24:44 +0800

    修改router-link

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,57 +1,64 @@
 <template>
   <div>
-    <mt-header class="nav" fixed title="myShop"></mt-header>    
+    <mt-header class="nav" fixed title="myShop"></mt-header>
     <router-view></router-view>
-    <mt-tabbar v-model="selected" fixed>
-      <mt-tab-item id="tab1">
-        <img slot="icon" src="../assets/cart.png">
-        <router-link to="./home">首页</router-link>
-      </mt-tab-item>
-      <mt-tab-item id="tab2">
-        <img slot="icon" src="../assets/cart.png">
-        <router-link to="./classify">分类</router-link>
-      </mt-tab-item>
-      <mt-tab-item id="tab3">
-        <img slot="icon" src="../assets/cart.png">
-        <router-link to="./cart">购物车</router-link>
-      </mt-tab-item>
-      <mt-tab-item id="tab4">
-        <img slot="icon" src="../assets/cart.png">
-        <router-link to="./me">我的</router-link>
-      </mt-tab-item>
-    </mt-tabbar>
+    <footer class="footer">
+      <router-link to="./home">首页</router-link>
+      <router-link to="./classify">分类</router-link>
+      <router-link to="./cart">购物车</router-link>
+      <router-link to="./me">我的</router-link>
+    </footer>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import { Tabbar, TabItem, Header } from 'mint-ui';
+import Vue from "vue";
+import { Tabbar, TabItem, Header } from "mint-ui";
 Vue.component(Tabbar.name, Tabbar);
 Vue.component(TabItem.name, TabItem);
 Vue.component(Header.name, Header);
 
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
-      selected: '首页'
+      selected: "首页"
     };
   },
   methods: {
-    test () {
-      alert('11111');
+    test() {
+      alert("11111");
     }
   },
   watch: {
-    selected: function (val, oldVal) {
+    selected: function(val, oldVal) {
       // 这里就可以通过 val 的值变更来确定
     }
   }
 };
 </script>
 
-<style>
+<style <style lang="less" scoped>
 .nav {
   z-index: 100px;
 }
+.active {
+  background: red;
+}
+.footer {
+  width: 100%;
+  position: fixed;
+  bottom: 0px;
+  > a {
+    display: inline-block;
+    width: 24%;
+    text-align: center;
+    color: black;
+    line-height:30px;
+  }
+}
+.router-link-active {
+  text-decoration: none;
+}
 </style>
+
